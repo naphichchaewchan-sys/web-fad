ed04a44af60e4626df47d39e6f1bff2029b88b8e
naphich <naphichchaewchan@email.com>
2025-10-05 13:35:48 +0700

    edit topbar.jsx

--- src/GUI/topBar.jsx
+++ src/GUI/topBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function topBar() {
        return (
-       <div className='div-2'><input type='text' name='in-1'/><button>ค้นหา</button></div>
+       <div className='div-2'><input type='text' /><button>ค้นหา</button></div>
        )
 }
 
